89a42e84e2b905f9e6c7a5941bd9472da9319fdf
Dercypt <quitalegfra@gmail.com>
2026-09-25 21:22:10 +0800

    refactor(src): add central module barrel export

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from './app';
+export * from './features/contacts';
+export * from './features/dispatch';
+export * from './features/history';
+export * from './features/location';
+export * from './features/settings';
+export * from './features/sos';
