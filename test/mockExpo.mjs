e4009bb545a3d12113d087379155e9c05bffec7d
Dercypt <quitalegfra@gmail.com>
2026-09-26 23:48:32 +0800

    test(sos): add ForegroundService and permission mock bindings to mockExpo

--- test/mockExpo.mjs
+++ test/mockExpo.mjs
@@ -1,8 +1,43 @@
-export class NativeModule {}
+export class EventEmitter {
+  addListener() { return { remove: () => {} }; }
+  removeListeners() {}
+  emit() {}
+}
+export class NativeModule extends EventEmitter {}
+export class UnavailabilityError extends Error {
+  constructor(moduleName, propertyName) {
+    super(`The method or property ${moduleName}.${propertyName} is not available.`);
+    this.code = 'ERR_UNAVAILABLE';
+  }
+}
+export const Platform = {
+  OS: 'android',
+  select: (obj) => obj.android || obj.default,
+};
+export const PermissionStatus = {
+  GRANTED: 'granted',
+  UNDETERMINED: 'undetermined',
+  DENIED: 'denied',
+};
+export function createPermissionHook() {
+  return () => [{ status: PermissionStatus.GRANTED }, async () => ({ status: PermissionStatus.GRANTED })];
+}
+export function isRunningInExpoGo() {
+  return false;
+}
 export function requireNativeModule(name) {
   return {
     isAvailableAsync: async () => false,
     sendSilentSms: async () => false,
+    startService: async () => true,
+    stopService: async () => true,
+    updateNotification: async () => true,
+    acquireWakeLock: async () => true,
+    releaseWakeLock: async () => true,
+    isServiceRunning: async () => false,
+    isWakeLockActive: async () => false,
+    addListener: () => ({ remove: () => {} }),
+    removeListeners: () => {},
   };
 }
 export function registerWebModule(moduleClass, name) {
